6adb2f61785afc152f1d8691b6d2c73b9fdbc84a
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2020-12-06 15:19:42 +0100

    Update rng.py

--- rng.py
+++ rng.py
@@ -9,3 +9,33 @@ x = datetime.now().microsecond / 1000000
 for i in range(1,50):
 	x = x * 4 * (1 - x)
 	print(expansor(x,20))
+
+	
+	
+	
+	
+import matplotlib.pyplot as plt 
+
+y=[]
+count=[]
+frame = 200
+up = 60
+down = -60
+
+i=0
+x = datetime.now().microsecond / 1000000
+
+while True:
+	i+=1
+	plt.axis([0, i, down, up])
+	count.append(i)
+	x = x * 4 * (1 - x)
+	y.append(expansor(x,10))
+	if len(count) >= frame:
+		plt.axis([i-frame, i, down, up])
+		count.pop(0)
+		y.pop(0)
+	plt.plot(count, y, 'r-')
+	plt.pause(0.05)
+
+#plt.show()
