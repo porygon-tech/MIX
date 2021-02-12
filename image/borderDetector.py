116de1d2b3fb2cd839ca368333e1944b483de330
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2021-02-12 10:27:11 +0100

    Some comments

--- image/borderDetector.py
+++ image/borderDetector.py
@@ -21,18 +21,20 @@ width = pic.shape[1]
 maxContrast = np.sqrt(3*(255)**2)
 print('detecting borders...')
 
+# generation of DownUpRightLeft pixel-displaced matrices.
+# they are basically the original image one-pixel displaced in each four directions.
 D = np.delete((np.insert(pic, height, [0,0,0], axis=0)), 0, axis=0)
 U = np.delete((np.insert(pic, 0, [0,0,0], axis=0)), height, axis=0)
 R = np.delete((np.insert(pic, width, [0,0,0], axis=1)), 0, axis=1) 
 L = np.delete((np.insert(pic, 0, [0,0,0], axis=1)), width, axis=1)
 
+# colour distance with the four most adjacent pixels.
 D_dist = np.sqrt(np.sum(((pic - D)**2), axis=2))
 U_dist = np.sqrt(np.sum(((pic - U)**2), axis=2))
 R_dist = np.sqrt(np.sum(((pic - R)**2), axis=2))
 L_dist = np.sqrt(np.sum(((pic - L)**2), axis=2))
 
- #optimal some decimals around 1
-
+# some math shit for contrast calculation
 def augment(x, slope):
 	return((-1 / slope**x + 1)*255)
 
